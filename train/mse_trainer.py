553544735b7625f0118a2a4d04364519a03f28c9
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-26 05:39:55 +0000

    Update, finished training on Google Cloud

--- train/mse_trainer.py
+++ train/mse_trainer.py
@@ -93,7 +93,7 @@ class Trainer:
                 data = data.cuda()
             data = Variable(data, volatile=True)
             recon_batch, mu, logvar = self.model(data)
-            test_loss += self.loss(recon_batch, data, mu, logvar).data[0]
+            test_loss += self.loss(recon_batch, data, mu, logvar).item()
             if i % 50 == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
